9b2a1462dad18c153971de0e7b54479183e16ca9
lit <103055119+EricaAnnor@users.noreply.github.com>
2024-07-30 20:49:47 +0000

    feat(web): Design checkout page

---
+++ src/userpages/checkout.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Table from './components/checkoutTable';
+import { IoArrowBackCircle } from "react-icons/io5";
+
+const Checkout = () => {
+  return (
+    <div className=''>
+      <div className='mx-[100px] mt-[90px] mb-[80px]'></div>
+
+      <div className='flex md:flex-row flex-col md:items-start items-center justify-around px-[100px] mb-20 space-y-20 '>
+        <div>
+          <p className='text-[40px] font-bold text-black mb-[70px]'>Shopping Cart.</p>
+          <Table />
+            <button className='w-[200px] h-[40px] mx-5 border border-black rounded-3xl text-black flex items-center justify-center'>
+              <IoArrowBackCircle className='mr-2' />
+              Continue shopping
+            </button>
+        </div>
+        <div className='w-[300px] h-[600px] rounded-xl bg-[#127816] flex flex-col py-8'>
+          <p className='text-[28px] text-white font-bold flex justify-center'>Payment info</p>
+          <div className='border-b my-6 border-gray-300 mx-3'></div>
+          <div className='flex flex-col space-y-3 justify-start'>
+            <p className='text-[14px] font-medium mb-4 px-9 text-white'>Payment method</p>
+            <button className='w-[160px] h-[40px] mx-5 border border-white rounded-3xl text-white'>
+              Mobile money
+            </button>
+            <p className='text-[14px] font-medium px-9 text-white'>MTN</p>
+            <p className='text-[14px] font-medium px-9 text-white'>Tele cash</p>
+            <p className='text-[14px] font-medium px-9 text-white'>AT money</p>
+          </div>
+          <div className='border-b my-6 border-gray-300 mx-3'></div>
+          <button className='w-[200px] h-[40px] mx-5 border border-white rounded-3xl text-white'>
+            Payment on delivery
+          </button>
+          <button className='w-[250px] h-[50px] mx-5 mt-[100px] bg-white text-[#127816] rounded-3xl'>
+            Checkout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ src/userpages/components/checkoutTable.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import watch from "../../assets/air.png";
+
+const Table = () => {
+  const initialData = [
+    { product: 'Product A', image: watch, quantity: 10, price: 20 },
+    { product: 'Product B', image: watch, quantity: 15, price: 30 },
+  ];
+
+  const [data, setData] = useState(initialData);
+
+  const handleIncrease = (index) => {
+    const newData = [...data];
+    newData[index].quantity += 1;
+    setData(newData);
+  };
+
+  const handleDecrease = (index) => {
+    const newData = [...data];
+    if (newData[index].quantity > 0) {
+      newData[index].quantity -= 1;
+    }
+    setData(newData);
+  };
+
+  const totalQuantity = data.reduce((acc, item) => acc + item.quantity, 0);
+  const totalPrice = data.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  return (
+    <table className="md:w-[700px] w-[400px] border-collapse mx-auto">
+      <thead>
+        <tr>
+          <th className="p-2 text-center">Product</th>
+          <th className="p-2 text-center">Quantity</th>
+          <th className="p-2 text-center">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index} className="border-t border-b">
+            <td className="p-2 flex items-center justify-center">
+              <img src={item.image} alt={item.product} className="mr-5 w-[110px] h-[110px]" />
+              <span>{item.product}</span>
+            </td>
+            <td className="p-2 text-center ">
+              <div className="border-2 rounded-2xl w-[80px] px-4 py-2 flex justify-center items-center mx-auto">
+                <button onClick={() => handleDecrease(index)} className="px-2">-</button>
+                <span className="px-2">{item.quantity}</span>
+                <button onClick={() => handleIncrease(index)} className="px-2">+</button>
+              </div>
+            </td>
+            <td className="p-2 text-center">Gh{item.price}</td>
+          </tr>
+        ))}
+        <tr className="border-t">
+          <td className="p-2 text-center"></td>
+          <td className="p-2 text-center font-bold"></td>
+          <td className="p-2 text-center">
+            <div className="flex flex-col justify-center items-center">
+              <span className='font-bold text-[25px]'>Total</span>
+              <span>Gh{totalPrice}</span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
